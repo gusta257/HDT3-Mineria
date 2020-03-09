de472001929366f505e38281d3c8e6d2861c111d
Gustavo de Leon <gdeleontobias@gmail.com>
2020-03-09 00:11:11 -0600

    importo csv

--- HDT3.R
+++ HDT3.R
@@ -1,6 +1,9 @@
-
+setwd("C:/Users/Gustavo/Desktop/SEPTIMO SEMESTRE/MINERIA/HDT3/HDT3-Mineria")
 library(rpart)
 library(caret)
 library(tree)
 library(rpart.plot)
 library(randomForest)
+
+test <- read.csv("test.csv", stringsAsFactors = FALSE)
+train <- read.csv("train.csv", stringsAsFactors = FALSE)
